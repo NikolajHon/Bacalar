c93e60c6e077a4d9eff69d9d9721d87984487eb9
NikolajHon <goncharenkokolya3@gmail.com>
2024-09-17 13:19:43 +0200

    modify styles

--- Server/front/src/components/forum/NewDiscussionForm.jsx
+++ Server/front/src/components/forum/NewDiscussionForm.jsx
@@ -37,7 +37,7 @@ const NewDiscussionForm = ({ onSuccess }) => {
                 placeholder="Title" 
                 required 
             />
-            <textarea 
+            <input 
                 value={content} 
                 onChange={(e) => setContent(e.target.value)} 
                 placeholder="Content" 
